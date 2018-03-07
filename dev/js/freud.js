35fd8dc91cfa2e082f09498b5353f235a44b1c46
rtartaglia <tartaglia.riccardo@gmail.com>
2018-03-07 17:10:44 +0100

    Initial commit

--- dev/js/freud.js
+++ dev/js/freud.js
@@ -6,7 +6,9 @@ class Freud {
     constructor() {
         // Params
         this._el = document.getElementById('freud')
+        this._vars = {}
         this._nodes = {}
+        this._regex = /[{]{2}(\S*)[}]{2}/g;
 
         // Events
     }
@@ -38,18 +40,38 @@ class Freud {
     createQuoteBot(node) {
         let self = this
         window.setTimeout(function () {
+            //creo i div di conversazione
             let quote = document.createElement('div')
-            quote.classList.add('freud-quote','__bot')
+            quote.classList.add('freud-quote', '__bot')
             let sentence = document.createElement('div')
             sentence.classList.add('freud-sentence')
-
+            //ecco il blocco di frasi
             quote.appendChild(sentence)
-
+            //appendo tutto al div contenitore
             self._el.appendChild(quote)
 
             node.sentence.map((v) => {
                 let element = document.createElement('div')
                 element.classList.add('__elem')
+                // sostituisco i placeholder {{}} con il valore della variabile richiesto
+                let m, findgr
+                while ((m = self._regex.exec(v.mex)) !== null) {
+                    // per evitare loop infiniti se non trova match
+                    if (m.index === self._regex.lastIndex) {
+                        regex.lastIndex++;
+                    }
+                    // eseguo i risultati, quando il gruppo e 0 trova {{nome}} mentre a gruppo 1 trova "nome"
+                    m.forEach((match, groupIndex) => {
+                        if (groupIndex === 0) {
+                            // trovo il placeholder completo
+                            findgr = match;
+                        } else {
+                            //sostituisco il placeholder con la variabile
+                            v.mex = v.mex.replace(findgr, self.getVar(match))
+                        }
+                    });
+                }
+                // faccio l'append del messaggio
                 element.innerHTML = v.mex;
                 sentence.appendChild(element)
                 window.setTimeout(function () {
@@ -57,37 +79,65 @@ class Freud {
                 }, v.wait)
             })
 
-
             let element = document.createElement('div')
             element.classList.add('__r')
-            if (node.r.type === 'button') {
-                node.r.el.map(v => {
-                    let btn = document.createElement('a')
-                    btn.classList.add('freud-btn')
-                    switch (v.type) {
-                        case 'nav':
-                            btn.onclick = () => {
-                                element.style.transform = "scale(0)"
-                                self.createQuoteHuman(v.label)
-                                self.do(v.value)
+            if (node.r !== undefined) {
+                switch (node.r.type) {
+                    case 'button':
+                        node.r.el.map(v => {
+                            let btn = document.createElement('a')
+                            btn.classList.add('freud-btn')
+                            switch (v.type) {
+                                case 'nav':
+                                    btn.onclick = () => {
+                                        element.style.transform = "scale(0)"
+                                        element.style.display = 'none'
+                                        self.createQuoteHuman(v.label)
+                                        self.do(v.value)
+                                    }
+                                    break
+                                case 'action':
+                                    btn.onclick = () => {
+                                        element.style.transform = "scale(0)"
+                                        element.style.display = 'none'
+                                        v.value()
+                                    }
+                                    break
+                                default:
+                                    btn.setAttribute('href', v.value)
+                                    btn.setAttribute('target', "__blank")
+                                    break
                             }
-                            break
-                        case 'action':
-                            btn.onclick = () => {
-                                element.style.transform = "scale(0)"
-                                v.value()
+                            btn.innerHTML = v.label;
+                            element.appendChild(btn)
+                        })
+                        sentence.appendChild(element)
+                        break;
+                    case 'input':
+                        node.r.el.map(v => {
+                            let input = document.createElement('input')
+                            input.classList.add('freud-input')
+                            input.setAttribute('placeholder', v.label);
+                            input.setAttribute('type', 'text');
+                            input.setAttribute('value', '');
+                            input.onkeypress = function (e) {
+                                if (!e) e = window.event;
+                                var keyCode = e.keyCode || e.which;
+                                if (keyCode == '13') {
+                                    v.fn(this.value);
+                                    element.style.transform = "scale(0)"
+                                    element.style.display = 'none'
+                                    self.createQuoteHuman(this.value)
+                                    return false;
+                                }
                             }
-                            break
-                        default:
-                            btn.setAttribute('href', v.value)
-                            btn.setAttribute('target', "__blank")
-                            break
-                    }
-                    btn.innerHTML = v.label;
-                    element.appendChild(btn)
-                })
-                sentence.appendChild(element)
-
+                            element.appendChild(input)
+                        })
+                        sentence.appendChild(element)
+                        break;
+                    default:
+                        break;
+                }
             }
 
             window.setTimeout(function () {
@@ -96,8 +146,8 @@ class Freud {
 
 
             let box = quote.getBoundingClientRect();
-            document.getElementById('bot-icon').style.top = (box.top + pageYOffset - 30) + "px"
-            window.scrollTo(0,document.body.scrollHeight);
+            document.getElementById('bot-icon').style.top = (box.top + pageYOffset - 22) + "px"
+            window.scrollTo(0, document.body.scrollHeight);
 
         }, node.wait)
     }
@@ -125,8 +175,8 @@ class Freud {
 
         let box = quote.getBoundingClientRect();
         document.getElementById('user-icon').style.opacity = '1'
-        document.getElementById('user-icon').style.top = (box.top + pageYOffset - 30) + "px"
-        window.scrollTo(0,document.body.scrollHeight);
+        document.getElementById('user-icon').style.top = (box.top + pageYOffset  - 22) + "px"
+        window.scrollTo(0, document.body.scrollHeight);
     }
 
     /**
@@ -178,8 +228,16 @@ class Freud {
         }
     }
 
-    moveIcon(icon){
+    moveIcon(icon) {
 
+    }
+
+    setVar(key, variabile) {
+        this._vars[key] = variabile;
+    }
+
+    getVar(key) {
+        return this._vars[key]
     }
 
     get nodes() {
--- dist/js/freud.js
+++ dist/js/freud.js
@@ -15,7 +15,9 @@ var Freud = function () {
 
         // Params
         this._el = document.getElementById('freud');
+        this._vars = {};
         this._nodes = {};
+        this._regex = /[{]{2}(\S*)[}]{2}/g;
 
         // Events
     }
@@ -57,18 +59,39 @@ var Freud = function () {
         value: function createQuoteBot(node) {
             var self = this;
             window.setTimeout(function () {
+                //creo i div di conversazione
                 var quote = document.createElement('div');
                 quote.classList.add('freud-quote', '__bot');
                 var sentence = document.createElement('div');
                 sentence.classList.add('freud-sentence');
-
+                //ecco il blocco di frasi
                 quote.appendChild(sentence);
-
+                //appendo tutto al div contenitore
                 self._el.appendChild(quote);
 
                 node.sentence.map(function (v) {
                     var element = document.createElement('div');
                     element.classList.add('__elem');
+                    // sostituisco i placeholder {{}} con il valore della variabile richiesto
+                    var m = void 0,
+                        findgr = void 0;
+                    while ((m = self._regex.exec(v.mex)) !== null) {
+                        // per evitare loop infiniti se non trova match
+                        if (m.index === self._regex.lastIndex) {
+                            regex.lastIndex++;
+                        }
+                        // eseguo i risultati, quando il gruppo e 0 trova {{nome}} mentre a gruppo 1 trova "nome"
+                        m.forEach(function (match, groupIndex) {
+                            if (groupIndex === 0) {
+                                // trovo il placeholder completo
+                                findgr = match;
+                            } else {
+                                //sostituisco il placeholder con la variabile
+                                v.mex = v.mex.replace(findgr, self.getVar(match));
+                            }
+                        });
+                    }
+                    // faccio l'append del messaggio
                     element.innerHTML = v.mex;
                     sentence.appendChild(element);
                     window.setTimeout(function () {
@@ -78,33 +101,63 @@ var Freud = function () {
 
                 var element = document.createElement('div');
                 element.classList.add('__r');
-                if (node.r.type === 'button') {
-                    node.r.el.map(function (v) {
-                        var btn = document.createElement('a');
-                        btn.classList.add('freud-btn');
-                        switch (v.type) {
-                            case 'nav':
-                                btn.onclick = function () {
-                                    element.style.transform = "scale(0)";
-                                    self.createQuoteHuman(v.label);
-                                    self.do(v.value);
+                if (node.r !== undefined) {
+                    switch (node.r.type) {
+                        case 'button':
+                            node.r.el.map(function (v) {
+                                var btn = document.createElement('a');
+                                btn.classList.add('freud-btn');
+                                switch (v.type) {
+                                    case 'nav':
+                                        btn.onclick = function () {
+                                            element.style.transform = "scale(0)";
+                                            element.style.display = 'none';
+                                            self.createQuoteHuman(v.label);
+                                            self.do(v.value);
+                                        };
+                                        break;
+                                    case 'action':
+                                        btn.onclick = function () {
+                                            element.style.transform = "scale(0)";
+                                            element.style.display = 'none';
+                                            v.value();
+                                        };
+                                        break;
+                                    default:
+                                        btn.setAttribute('href', v.value);
+                                        btn.setAttribute('target', "__blank");
+                                        break;
+                                }
+                                btn.innerHTML = v.label;
+                                element.appendChild(btn);
+                            });
+                            sentence.appendChild(element);
+                            break;
+                        case 'input':
+                            node.r.el.map(function (v) {
+                                var input = document.createElement('input');
+                                input.classList.add('freud-input');
+                                input.setAttribute('placeholder', v.label);
+                                input.setAttribute('type', 'text');
+                                input.setAttribute('value', '');
+                                input.onkeypress = function (e) {
+                                    if (!e) e = window.event;
+                                    var keyCode = e.keyCode || e.which;
+                                    if (keyCode == '13') {
+                                        v.fn(this.value);
+                                        element.style.transform = "scale(0)";
+                                        element.style.display = 'none';
+                                        self.createQuoteHuman(this.value);
+                                        return false;
+                                    }
                                 };
-                                break;
-                            case 'action':
-                                btn.onclick = function () {
-                                    element.style.transform = "scale(0)";
-                                    v.value();
-                                };
-                                break;
-                            default:
-                                btn.setAttribute('href', v.value);
-                                btn.setAttribute('target', "__blank");
-                                break;
-                        }
-                        btn.innerHTML = v.label;
-                        element.appendChild(btn);
-                    });
-                    sentence.appendChild(element);
+                                element.appendChild(input);
+                            });
+                            sentence.appendChild(element);
+                            break;
+                        default:
+                            break;
+                    }
                 }
 
                 window.setTimeout(function () {
@@ -112,7 +165,7 @@ var Freud = function () {
                 }, node.r.wait);
 
                 var box = quote.getBoundingClientRect();
-                document.getElementById('bot-icon').style.top = box.top + pageYOffset - 30 + "px";
+                document.getElementById('bot-icon').style.top = box.top + pageYOffset - 22 + "px";
                 window.scrollTo(0, document.body.scrollHeight);
             }, node.wait);
         }
@@ -142,7 +195,7 @@ var Freud = function () {
 
             var box = quote.getBoundingClientRect();
             document.getElementById('user-icon').style.opacity = '1';
-            document.getElementById('user-icon').style.top = box.top + pageYOffset - 30 + "px";
+            document.getElementById('user-icon').style.top = box.top + pageYOffset - 22 + "px";
             window.scrollTo(0, document.body.scrollHeight);
         }
 
@@ -212,6 +265,16 @@ var Freud = function () {
     }, {
         key: 'moveIcon',
         value: function moveIcon(icon) {}
+    }, {
+        key: 'setVar',
+        value: function setVar(key, variabile) {
+            this._vars[key] = variabile;
+        }
+    }, {
+        key: 'getVar',
+        value: function getVar(key) {
+            return this._vars[key];
+        }
     }, {
         key: 'nodes',
         get: function get() {
